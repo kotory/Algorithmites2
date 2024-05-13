732827c49e10084f3e6538b53b72cfb2f9cff2f8
kotory <twitch320@gmail.com>
2024-05-13 21:27:32 +0300

    правочки

--- findClosestSquare/src/Main.java
+++ findClosestSquare/src/Main.java
@@ -13,22 +13,18 @@ public class Main {
         System.out.println("Число, квадрат которого будет являться ближайшим к заданному числу: " + closestSquare);
     }
 
-    // Метод для нахождения ближайшего квадрата к заданному числу n
     public static int findClosestSquare(int n) {
         if (n <= 0) {
-            return 0; // если n не положительное, возвращаем 0 (невозможно найти квадрат)
+            return 0;
         }
 
-        // Инициализируем переменные для хранения ближайшего квадрата и его разницы с n
         int closestSquare = 0;
         int minDifference = Integer.MAX_VALUE;
 
-        // Перебираем все целые числа, начиная с 1, и находим квадраты ближе к n
         for (int i = 1; i <= n; i++) {
             int square = i * i;
             int difference = Math.abs(square - n);
 
-            // Если найдена новая более близкая разница, обновляем ближайший квадрат и разницу
             if (difference < minDifference) {
                 closestSquare = square;
                 minDifference = difference;
